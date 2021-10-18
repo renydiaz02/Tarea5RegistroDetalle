932db99ef47c5f5d43c5172f905a88c60073af37
renydiaz02 <reny_diaz@ucne.edu.do>
2021-10-18 03:48:03 -0400

    Tarea 5 Laboratorio RegistroDetalle

---
+++ DAL/Contexto.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroDetalle.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDetalle.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Permisos> Permisos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\Tarea5.db");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Permisos>().HasData(new Permisos
+            {
+                PermisoId = 1,
+                Nombre = "Permiso de salida",
+                Descripcion = "Permiso para que el trabajador pueda salir hacer una deligencia breve"
+            });
+
+            modelBuilder.Entity<Permisos>().HasData(new Permisos
+            {
+                PermisoId = 2,
+                Nombre = "Permiso de estudio",
+                Descripcion = "Permiso para que el trabajador pueda ir a la universidad a estudiar durante su jornada de trabajo"
+            });
+
+            modelBuilder.Entity<Permisos>().HasData(new Permisos
+            {
+                PermisoId = 3,
+                Nombre = "Permiso de viaje",
+                Descripcion = "Permiso para que el trabajador salga del pais mientras esta en labor"
+            });
+
+            modelBuilder.Entity<Permisos>().HasData(new Permisos
+            {
+                PermisoId = 4,
+                Nombre = "Permiso por enfermedad",
+                Descripcion = "Permiso para que el trabajador pueda ir al medico durante la jornada laboral"
+            });
+        }
+    }
+}
---
+++ Migrations/20211018071256_migracion.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroDetalle.Migrations
+{
+    public partial class migracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Permisos",
+                columns: table => new
+                {
+                    PermisoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permisos", x => x.PermisoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    RolId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true),
+                    esActivo = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.RolId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RolesDetalle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RolId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PermisoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    esAsignado = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RolesDetalle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RolesDetalle_Roles_RolId",
+                        column: x => x.RolId,
+                        principalTable: "Roles",
+                        principalColumn: "RolId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Permisos",
+                columns: new[] { "PermisoId", "Descripcion", "Nombre" },
+                values: new object[] { 1, "Permiso para que el trabajador pueda salir hacer una deligencia breve", "Permiso de salida" });
+
+            migrationBuilder.InsertData(
+                table: "Permisos",
+                columns: new[] { "PermisoId", "Descripcion", "Nombre" },
+                values: new object[] { 2, "Permiso para que el trabajador pueda ir a la universidad a estudiar durante su jornada de trabajo", "Permiso de estudio" });
+
+            migrationBuilder.InsertData(
+                table: "Permisos",
+                columns: new[] { "PermisoId", "Descripcion", "Nombre" },
+                values: new object[] { 3, "Permiso para que el trabajador salga del pais mientras esta en labor", "Permiso de viaje" });
+
+            migrationBuilder.InsertData(
+                table: "Permisos",
+                columns: new[] { "PermisoId", "Descripcion", "Nombre" },
+                values: new object[] { 4, "Permiso para que el trabajador pueda ir al medico durante la jornada laboral", "Permiso por enfermedad" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolesDetalle_RolId",
+                table: "RolesDetalle",
+                column: "RolId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Permisos");
+
+            migrationBuilder.DropTable(
+                name: "RolesDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
